8eddf9bac972d64f553380d9d4e4fc4626537a0f
Jan Carbonell <jcllobet@gmail.com>
2024-02-17 17:29:15 -0500

    checkpoint navbar

---
+++ app/components/date.tsx
@@ -0,0 +1,18 @@
+export default function Date() {
+  return (
+    <div className="py-12">
+      <div className="max-w-7xl mx-auto px-8">
+        <h3 className="font-cal text-5xl text-orange-100">
+          March 23-24th, 2024
+        </h3>
+        <p className="mt-4 font-light text-orange-300">
+          Saturday, March 23rd, 9:00 AM to Sunday, March 24th, 3:14 PM
+          <br />
+          Demo Fair (internal) March 24th, 3:45 PM
+          <br />
+          Public Demos & Awards: March 24th, 5:30 PM
+        </p>
+      </div>
+    </div>
+  );
+}
