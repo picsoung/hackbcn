6aabc2bd979422be2e644efa421d267e79b9add1
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-12 16:09:38 +0000

    Update date formatting and event descriptions in Dates component

--- app/components/Dates.tsx
+++ app/components/Dates.tsx
@@ -2,13 +2,13 @@ export default function Dates() {
   return (
     <div className="py-12">
       <div className="max-w-7xl mx-auto px-8">
-        <h3 className="font-cal text-5xl text-white">March 23-24th, 2024</h3>
+        <h3 className="font-cal text-5xl text-white">March 23-24, 2024</h3>
         <p className="mt-4 font-light text-white">
-          Saturday, March 23rd, 9:00 AM to Sunday, March 24th, 3:14 PM
+          Hacking: Saturday, March 23 at 9:00 AM to Sunday, March 24 at 3:14 PM
           <br />
-          Demo Fair (internal) March 24th, 3:45 PM
+          Internal Demo Fair: March 24, 3:45 PM
           <br />
-          Public Demos & Awards: March 24th, 5:30 PM
+          Public Demonstrations & Awards Ceremony: March 24, 5:30 PM
         </p>
       </div>
     </div>
