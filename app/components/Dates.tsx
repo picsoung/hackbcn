c6d12ca950a7ea8d85ab165186d8370dd771fbe6
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-12 16:23:12 +0000

    Update font color in Dates component

--- app/components/Dates.tsx
+++ app/components/Dates.tsx
@@ -2,8 +2,8 @@ export default function Dates() {
   return (
     <div className="py-12">
       <div className="max-w-7xl mx-auto px-8">
-        <h3 className="font-cal text-5xl text-white">March 23-24, 2024</h3>
-        <p className="mt-4 font-light text-white">
+        <h3 className="font-cal text-5xl text-black">March 23-24, 2024</h3>
+        <p className="mt-4 font-light text-black">
           Hacking: Saturday, March 23 at 9:00 AM to Sunday, March 24 at 3:14 PM
           <br />
           Internal Demo Fair: March 24, 3:45 PM
