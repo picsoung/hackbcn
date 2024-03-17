f60063d5a937e07926d69cfc6f3ff86d4973c6c9
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 14:47:14 -0400

    Update Sponsors component and add new logos

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -1,60 +1,71 @@
 export default function Sponsors() {
   return (
     <div id="sponsors" className="bg-white py-10 sm:py-10">
-      <div className="mx-auto max-w-7xl px-88 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 lg:px-8">
         <h2 className="mt-2 text-3xl sm:text-5xl font-cal font-semibold text-indigo-600">
           Sponsors
         </h2>
-        <div className="flex flex-col flex-wrap justify-center items-center mx-auto gap-8">
-          <div className="flex gap-8">
+        <div className="flex flex-col flex-wrap justify-center items-center mx-auto gap-4 mt-12">
+          <div className="flex flex-wrap gap-4 justify-center">
             <img
-              className="max-h-32 w-full object-contain bg-black"
+              className="h-auto max-h-32 object-contain bg-black"
               src="/logos/praxis2.jpg"
               alt="Praxis"
             />
             <img
-              className="max-h-32 w-full object-contain"
+              className="h-auto max-h-32 object-contain"
               src="/logos/modal2.jpg"
               alt="Modal"
             />
+            <img
+              className="h-auto max-h-32 object-contain"
+              src="/logos/ctech2.png"
+              alt="Cornell Tech"
+            />
           </div>
 
-          <div className="flex flex-row gap-8">
+          <div className="flex flex-wrap gap-4 justify-center">
             <img
-              className="max-h-32 w-full object-contain"
+              className="h-auto max-h-32 object-contain"
               src="/logos/meta2.jpg"
               alt="Meta"
             />
             <img
-              className="max-h-32 w-full object-contain"
+              className="h-auto max-h-32 object-contain"
               src="/logos/warp2.jpg"
               alt="Warp"
             />
             <img
-              className="max-h-32 w-full object-contain"
-              src="/logos/warp2.jpg"
+              className="h-auto max-h-32 object-contain"
+              src="/logos/lastmileai2.jpeg"
               alt="Warp"
             />
             <img
-              className="max-h-32 w-full object-contain"
-              src="/logos/warp2.jpg"
+              className="h-auto max-h-32 object-contain"
+              src="/logos/nomic2.jpg"
+              alt="Warp"
+            />
+            <img
+              className="h-auto max-h-32 object-contain"
+              src="/logos/prompt2.png"
               alt="Warp"
             />
           </div>
-          <div className="flex flex-wrap">
+          
+          <div className="flex flex-wrap gap-4 justify-center">
             <img
-              className="max-h-32 w-full object-contain"
+              className="h-auto max-h-32 object-contain"
               src="/logos/cornell.jpg"
               alt="Cornell"
             />
             <img
-              className="max-h-32 w-full object-contain"
+              className="h-auto max-h-32 object-contain"
               src="/logos/columbia.jpg"
               alt="Columbia"
             />
             <img
-              className="max-h-32 w-full object-contain scale-150"
-              src="/logos/nyu.png"
+              className="h-auto max-h-32 object-contain"
+              src="/logos/nyu.jpg"
               alt="NYU"
             />
           </div>
@@ -63,10 +74,3 @@ export default function Sponsors() {
     </div>
   );
 }
-// •⁠  ⁠Praxis (praxisnation.com)
-// •⁠  ⁠Meta (meta.com)
-// •⁠  ⁠Modal (modal.com)
-// •⁠  ⁠Warp (warp.dev)
-// •⁠  ⁠Cornell
-// •⁠  ⁠NYU
-// •⁠  ⁠Columbia
