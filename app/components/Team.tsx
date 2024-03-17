f0e3cb44fb217193ff1388b0d453b4d6a684da02
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 10:08:39 -0400

    Update Team component layout

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -7,8 +7,8 @@ export default function Team() {
             Organizing Team
           </h2>
         </div>
-        <div className="flex mt-6 text-gray-800 text-lg">
-          <div className="w-1/2 mr-8">
+        <div className="flex flex-col sm:flex-row mt-6 text-gray-800 text-lg">
+          <div className="w-full sm:w-1/2 mr-8">
             <div>IvyHacks is brought to you by:</div>
             <br />
             <div>
@@ -37,7 +37,7 @@ export default function Team() {
             This hackathon wouldn&apos;t be possible without the incredible
             support of our universities, our sponsors, volunteers and YOU!
           </div>
-          <div className="w-1/2 ml-8">
+          <div className="w-full sm:w-1/2 mt-6 sm:mt-0">
             Feel free to reach out if you have any questions:
             <br />
             andrew.siah@columbia.edu
