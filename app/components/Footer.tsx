2b3eb1a3468c25481ccc3f124eb7e3d083d6a5b6
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 09:38:17 -0400

    Update Footer component styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,13 +4,11 @@ export default function Footer({ padding = false }) {
       <div
         className={`mx-auto max-w-7xl ${
           padding ? "px-6 lg:px-8" : ""
-        } py-12 md:flex md:items-center md:justify-between`}
+        } py-6 md:flex md:items-center md:justify-between`}
       >
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-xs leading-5 text-white">
+          <p className="text-center text-lg leading-5 text-white">
             &copy; 2024. Brought to you from NYC with{" "}
-            <span style={{ color: "red" }}>&#x2764;</span>
-            <span style={{ color: "red" }}>&#x2764;</span>
             <span style={{ color: "red" }}>&#x2764;</span> from <b>Columbia</b>{" "}
             x <b>Cornell</b> x <b>NYU</b>.
           </p>
