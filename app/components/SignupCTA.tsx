d9a154c73156b50463fc4b668f6840d7eeb7bb8e
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 09:34:06 -0400

    Update SignupCTA component styling

--- app/components/SignupCTA.tsx
+++ app/components/SignupCTA.tsx
@@ -5,10 +5,10 @@ export default function SignupCTA() {
     <div className="py-12">
       <div className="flex max-w-7xl mx-auto px-8">
         <div className="w-2/3">
-          <h3 className="font-cal text-5xl text-white">
+          <h3 className="mt-2 text-3xl sm:text-5xl font-cal font-semibold text-white">
             Join us at IvyHacks 2024
           </h3>
-          <p className="mt-4 font-light text-white">
+          <p className="mt-4 text-xl text-gray-200">
             Sign up in minutes and get ready to hack!
           </p>
         </div>
