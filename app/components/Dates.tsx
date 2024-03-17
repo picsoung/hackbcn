fb501fc05672714b11ad3844e090466c31071c99
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 09:15:29 -0400

    Update Dates component styling and content

--- app/components/Dates.tsx
+++ app/components/Dates.tsx
@@ -1,14 +1,21 @@
 export default function Dates() {
   return (
-    <div className="py-12">
+    <div className="py-10">
       <div className="max-w-7xl mx-auto px-8">
-        <h3 className="font-cal text-5xl text-black">March 23-24, 2024</h3>
-        <p className="mt-4 font-light text-black">
-          Hacking: Saturday, March 23 at 9:00 AM to Sunday, March 24 at 3:14 PM
+        <h3 className="text-3xl sm:text-5xl font-cal font-semibold text-indigo-600">
+          March 23-24, 2024
+        </h3>
+        <p className="mt-4 text-xl text-gray-200">
+          <span className="font-semibold">Hacking: </span>Saturday, March 23 at
+          9:00 AM to Sunday, March 24 at 3:14 PM
           <br />
-          Internal Demo Fair: March 24, 3:45 PM
+          <span className="font-semibold">Internal Demo Fair: </span>
+          March 24, 3:45 PM
           <br />
-          Public Demonstrations & Awards Ceremony: March 24, 5:30 PM
+          <span className="font-semibold">
+            Public Demonstrations & Awards Ceremony:{" "}
+          </span>
+          March 24, 5:30 PM
         </p>
       </div>
     </div>
