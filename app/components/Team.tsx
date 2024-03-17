0d81142ab40eea3e54c7d431322f7301987487fb
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 10:03:27 -0400

    Update Team component styling

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -1,13 +1,13 @@
 export default function Team() {
   return (
-    <div id="about" className="bg-white py-24 sm:py-32">
+    <div id="about" className="bg-white py-10 sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="max-w-2xl">
-          <h2 className="mt-2 text-3xl font-cal text-indigo-600 sm:text-5xl">
+          <h2 className="mt-1 text-3xl sm:text-5xl font-cal font-semibold text-indigo-600">
             Organizing Team
           </h2>
         </div>
-        <div className="flex mt-6 text-gray-700">
+        <div className="flex mt-6 text-gray-800 text-lg">
           <div className="w-1/2 mr-8">
             <div>IvyHacks is brought to you by:</div>
             <br />
@@ -23,28 +23,22 @@ export default function Team() {
                     key={index}
                     style={{
                       display: "grid",
-                      gridTemplateColumns: "1fr 1fr",
+                      gridTemplateColumns: "1fr",
                       marginBottom: "0.5rem",
+                      marginLeft: "5rem",
                     }}
                   >
-                    <span style={{ justifySelf: "end", paddingRight: "1rem" }}>
-                      {name.firstName}
-                    </span>
-                    <span style={{ justifySelf: "start" }}>
-                      {name.lastName}
-                    </span>
+                    {name.firstName} {name.lastName}
                   </li>
                 ))}
               </ul>
             </div>
-            <br />
             <br />
             This hackathon wouldn&apos;t be possible without the incredible
             support of our universities, our sponsors, volunteers and YOU!
           </div>
           <div className="w-1/2 ml-8">
             Feel free to reach out if you have any questions:
-            <br />
             <br />
             andrew.siah@columbia.edu
             <br />
