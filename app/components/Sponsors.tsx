aa0a0664f568a3713ef5c015d75d594487c6345e
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 11:33:29 -0400

    Refactor Sponsors component layout

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -1,52 +1,63 @@
 export default function Sponsors() {
   return (
     <div id="sponsors" className="bg-white py-10 sm:py-10">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-88 lg:px-8">
         <h2 className="mt-2 text-3xl sm:text-5xl font-cal font-semibold text-indigo-600">
           Sponsors
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-5 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-5 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
-            className="col-span-2 col-start-2 max-h-32 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="/logos/free_agency2.jpeg"
-            alt="Free Agency"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1 bg-black"
-            src="/logos/praxis2.jpg"
-            alt="Praxis"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
-            src="/logos/modal2.jpg"
-            alt="Modal"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
-            src="/logos/meta2.jpg"
-            alt="Meta"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
-            src="/logos/warp2.jpg"
-            alt="Warp"
-          />
-          <img />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
-            src="/logos/cornell.jpg"
-            alt="Cornell"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
-            src="/logos/columbia.jpg"
-            alt="Columbia"
-          />
-          <img
-            className="col-span-2 max-h-32 w-full object-contain lg:col-span-1 scale-150"
-            src="/logos/nyu.png"
-            alt="NYU"
-          />
+        <div className="flex flex-col flex-wrap justify-center items-center mx-auto gap-8">
+          <div className="flex gap-8">
+            <img
+              className="max-h-32 w-full object-contain bg-black"
+              src="/logos/praxis2.jpg"
+              alt="Praxis"
+            />
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/modal2.jpg"
+              alt="Modal"
+            />
+          </div>
+
+          <div className="flex flex-row gap-8">
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/meta2.jpg"
+              alt="Meta"
+            />
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/warp2.jpg"
+              alt="Warp"
+            />
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/warp2.jpg"
+              alt="Warp"
+            />
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/warp2.jpg"
+              alt="Warp"
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/cornell.jpg"
+              alt="Cornell"
+            />
+            <img
+              className="max-h-32 w-full object-contain"
+              src="/logos/columbia.jpg"
+              alt="Columbia"
+            />
+            <img
+              className="max-h-32 w-full object-contain scale-150"
+              src="/logos/nyu.png"
+              alt="NYU"
+            />
+          </div>
         </div>
       </div>
     </div>
