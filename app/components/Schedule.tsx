80a6cc434de55bbe21213849873c28b057e4e178
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-17 09:31:05 -0400

    Update schedule styling

--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -6,42 +6,42 @@ const schedule = [
     title: "Saturday - March 23rd",
     content: (
       <>
-        <small className="font-bold uppercase text-orange-700">Morning</small>
+        <small className="font-bold uppercase text-indigo-600">Morning</small>
         <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           8:00 AM | Networking + Breakfast
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           9:00 AM | Opening Speeches
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           9:45 AM | Hacking solo or in teams, supported by mentors
         </time>
         <br />
+        <small className="font-bold uppercase text-indigo-600">Hack Time</small>
         <br />
-        <small className="font-bold uppercase text-orange-700">Hack Time</small>
-        <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           12:00 PM | Lunch
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           01:00 PM | More hacking
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           06:00 PM | Dinner
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           07:00 PM | Even more hacking
         </time>
         <br />
-        <br />
-        <small className="font-bold uppercase text-orange-700">
+        <small className="font-bold uppercase text-indigo-600">
           Periodically
         </small>
         <br />
-        Guest lectures by mentors and sponsors
+        <span className="font-medium text-gray-900">
+          Guest lectures by mentors and sponsors
+        </span>
         <br />
-        Snacks and surprises
+        <span className="font-medium text-gray-900">Snacks and surprises</span>
       </>
     ),
   },
@@ -49,39 +49,36 @@ const schedule = [
     title: "Sunday - March 24th",
     content: (
       <>
-        <small className="font-bold uppercase text-orange-700">Morning</small>
+        <small className="font-bold uppercase text-indigo-600">Morning</small>
         <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           09:00 AM | Breakfast
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           10:00 AM | Keep pushing
         </time>
         <br />
+        <small className="font-bold uppercase text-indigo-600">Midday</small>
         <br />
-        <small className="font-bold uppercase text-orange-700">Midday</small>
-        <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           12:00 PM | Lunch
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           03:14 PM | Submit repos and prepare for final presentations
         </time>
         <br />
+        <small className="font-bold uppercase text-indigo-600">Afternoon</small>
         <br />
-        <small className="font-bold uppercase text-orange-700">Afternoon</small>
-        <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           03:45 PM | Hackfair - Teams do their final pitches
         </time>
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           05:30 PM | Final Demos
         </time>
         <br />
+        <small className="font-bold uppercase text-indigo-600">Wrap-up</small>
         <br />
-        <small className="font-bold uppercase text-orange-700">Wrap-up</small>
-        <br />
-        <time className="block font-medium text-gray-500">
+        <time className="block font-medium text-gray-900">
           06:30 PM | Deliberation &amp; Awards
         </time>
       </>
@@ -92,9 +89,9 @@ const schedule = [
 export default function Schedule() {
   return (
     <div id="schedule" className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+      <div className="mx-auto max-w-7xl px-6 py-10 sm:py-10 lg:px-8 lg:py-10">
         <div className="mx-auto max-w-7xl divide-y divide-orange-900/10">
-          <h2 className="mt-2 text-3xl font-cal text-indigo-600 sm:text-5xl">
+          <h2 className="text-3xl sm:text-5xl font-cal font-semibold text-indigo-600">
             Schedule
           </h2>
           <dl className="mt-10 space-y-6 divide-y divide-indigo-600/10">
@@ -108,8 +105,8 @@ export default function Schedule() {
                 {({ open }) => (
                   <>
                     <dt>
-                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-orange-900">
-                        <span className="text-base font-semibold leading-7">
+                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                        <span className="text-lg font-bold leading-7">
                           {day.title}
                         </span>
                         <span className="ml-6 flex h-7 items-center">
