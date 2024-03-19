2ca72de0a7df56a9a7d8433dc6b2313714244319
Sudhanshu Pandey <sp6370@nyu.edu>
2024-03-19 00:09:49 -0400

    Add HF0.png logo

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -7,117 +7,163 @@ export default function Sponsors() {
         </h2>
         <div className="flex flex-col flex-wrap justify-center items-center mx-auto gap-4 mt-12">
           <div className="flex flex-wrap gap-4 justify-center">
-            <img
-              className="h-auto max-h-40 object-contain bg-black"
-              src="/logos/praxis2.jpg"
-              alt="Praxis"
-            />
-            <img
-              className="h-auto max-h-40 object-contain"
-              src="/logos/modal2.jpg"
-              alt="Modal"
-            />
-            <img
-              className="h-auto max-h-40 object-contain"
-              src="/logos/ctech2.png"
-              alt="Cornell Tech"
-            />
-          </div>
+              <a href="https://www.praxisnation.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-40 object-contain bg-black hover:cursor-pointer"
+                  src="/logos/praxis2.jpg"
+                  alt="Praxis"
+                />
+              </a>
+              <a href="https://modal.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-40 object-contain hover:cursor-pointer"
+                  src="/logos/modal2.jpg"
+                  alt="Modal"
+                />
+              </a>
+              <a href="https://tech.cornell.edu/jacobs-technion-cornell-institute/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-40 object-contain hover:cursor-pointer"
+                  src="/logos/ctech2.png"
+                  alt="Cornell Tech"
+                />
+              </a>
+            </div>
 
-          <div className="flex flex-wrap gap-4 justify-center mt-10">
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/Anthropic.png"
-              alt="Anthropic"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/hugging-face.jpeg"
-              alt="Hugging Face"/>
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/meta2.jpg"
-              alt="Meta"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/warp2.jpg"
-              alt="Warp"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/nomic2.jpg"
-              alt="Nomic"
-            />
-          </div>
+            <div className="flex flex-wrap gap-4 justify-center mt-10">
+              <a href="https://www.anthropic.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/Anthropic.png"
+                  alt="Anthropic"
+                />
+              </a>
+              <a href="https://huggingface.co/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/hugging-face.jpeg"
+                  alt="Hugging Face"
+                />
+              </a>
+              <a href="https://meta.com" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/meta2.jpg"
+                  alt="Meta"
+                />
+              </a>
+              <a href="https://warp.dev" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/warp2.jpg"
+                  alt="Warp"
+                />
+              </a>
+              <a href="https://www.nomic.ai/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/nomic2.jpg"
+                  alt="Nomic"
+                />
+              </a>
+            </div>
 
-          <div className="flex flex-wrap gap-4 justify-center">
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/lastmileai2.jpeg"
-              alt="LastMileAI"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/nyAILab.png"
-              alt="New York AI Labs"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/prompt2.png"
-              alt="PromptLayer"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/passes.png"
-              alt="Passes"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/samsung.jpg"
-              alt="Samsung Next"
-            />
-          </div>
+            <div className="flex flex-wrap gap-4 justify-center">
+              <a href="https://lastmileai.dev/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/lastmileai2.jpeg"
+                  alt="LastMileAI"
+                />
+              </a>
+              <img
+                className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                src="/logos/nyAILab.png"
+                alt="New York AI Labs"
+              />
+              <a href="https://promptlayer.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/prompt2.png"
+                  alt="PromptLayer"
+                />
+              </a>
+              <a href="https://www.passes.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/passes.png"
+                  alt="Passes"
+                />
+              </a>
+              <a href="https://www.samsungnext.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/samsung.jpg"
+                  alt="Samsung Next"
+                />
+              </a>
+            </div>
 
-          <div className="flex flex-wrap gap-4 justify-center mt-10">
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/645 Ventures.png"
-              alt="645 Ventures"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/everywhereVC.jpg"
-              alt="EverywhereVC"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/tolacapital.png"
-              alt="Tola Capital"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/free_agency2.jpeg"
-              alt="Free Agency"
-            />
-          </div>
-          
-          <div className="flex flex-wrap gap-4 justify-center">
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/cornell.jpg"
-              alt="Cornell"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/columbia.jpg"
-              alt="Columbia"
-            />
-            <img
-              className="h-auto max-h-32 object-contain"
-              src="/logos/nyu.jpg"
-              alt="NYU"
-            />
+            <div className="flex flex-wrap gap-4 justify-center mt-10">
+              <a href="https://645ventures.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/645 Ventures.png"
+                  alt="645 Ventures"
+                />
+              </a>
+              <a href="https://www.hf0.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/HF0.png"
+                  alt="HF0"
+                />
+              </a>
+              <a href="https://everywhere.vc/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/everywhereVC.jpg"
+                  alt="EverywhereVC"
+                />
+              </a>
+              <a href="https://tolacapital.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/tolacapital.png"
+                  alt="Tola Capital"
+                />
+              </a>
+              <a href="https://www.freeagency.com/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/free_agency2.jpeg"
+                  alt="Free Agency"
+                />
+              </a>
+            </div>
+            
+            <div className="flex flex-wrap gap-4 justify-center">
+              <a href="https://www.cornell.edu/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/cornell.jpg"
+                  alt="Cornell"
+                />
+              </a>
+              <a href="https://www.columbia.edu/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/columbia.jpg"
+                  alt="Columbia"
+                />
+              </a>
+              <a href="https://www.nyu.edu/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="h-auto max-h-32 object-contain hover:cursor-pointer"
+                  src="/logos/nyu.jpg"
+                  alt="NYU"
+                />
+              </a>
           </div>
         </div>
       </div>
