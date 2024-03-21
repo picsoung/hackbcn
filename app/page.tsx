78fb4594cc1f1182fb7c738f32bc93bbfa115a3b
LouisWMV <cuberoalouis@gmail.com>
2024-03-21 02:23:36 -0400

    Added Judges Section
    - Added functional "Judges" button to header.
    
    - Created "judges" folder for images.
    
    - Added 6 placeholder judges.
    (We love the rock)
    
    - Added black border around images

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import Sponsors from "./components/Sponsors";
+import Judges from "./components/Judges";
 import WhyJoin from "./components/WhyJoin";
 import Schedule from "./components/Schedule";
 import Dates from "./components/Dates";
@@ -18,6 +19,7 @@ export default function Home() {
     { name: "Why should I Join?", href: "#why" },
     { name: "Schedule", href: "#schedule" },
     { name: "Organizing Team", href: "#about" },
+    { name: "Judges", href: "#judges" },
   ];
 
   return (
@@ -30,6 +32,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col">
         <Hero />
         <Sponsors />
+        <Judges />
         <WhyJoin />
         <Dates />
         <Schedule />
