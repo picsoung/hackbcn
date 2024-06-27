514f80c52f99b30c1f4c4c797a15206b8a76a072
Nicolas Grenié <nicolas.grenie@gmail.com>
2024-06-27 12:38:24 +0200

    fix: add demo event link

--- app/components/ApplyButton.tsx
+++ app/components/ApplyButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useIntl } from "./Intl";
 
 export default function ApplyButton() {
@@ -14,7 +15,9 @@ export default function ApplyButton() {
 
   return (
     <>
-      <h3 className="inline-flex items-center px-3 py-2 border border-transparent text-lg font-medium rounded-md text-gray-200 bg-amber-800 hover:bg-indigo-700 hover:text-white">Application closed</h3>
+      <h3 className="inline-flex items-center px-3 py-2 border border-transparent text-lg font-medium rounded-md text-gray-200 bg-red-500 hover:bg-red-700 hover:text-white">Application closed</h3>
+      &nbsp;
+      <Link className="inline-flex items-center px-3 py-2 border border-transparent text-lg font-medium rounded-md text-gray-200 bg-green-800 hover:bg-geen-700 hover:text-white" href="https://lu.ma/10wnz112">Attend final demos</Link>
       {/* <button
       onClick={handleClick}
         className="inline-flex items-center px-3 py-2 border border-transparent text-lg font-medium rounded-md text-gray-200 bg-amber-500 hover:bg-indigo-700 hover:text-white"
